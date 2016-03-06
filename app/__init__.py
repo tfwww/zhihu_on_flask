bc9c62ed45eaf78c6a464fa3f3111ea09b0e5812
wmzhong <zhongwm.net@gmail.com>
2016-03-06 21:42:46 +0800

    app structure

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask.ext.bootstrap import Bootstrap
+
+def create_app():
+	app = Flask(__name__)
+	bootstrap = Bootstrap(app)
+
+	from .main import main as main_blueprint
+	app.register_blueprint(main_blueprint)
+
+	return app
