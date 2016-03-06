155a1a6db40cf832e7e29460b71b0bf5eca4ac15
wmzhong <zhongwm.net@gmail.com>
2016-03-06 13:36:32 +0800

    Reset

--- zhihu.py
+++ zhihu.py
@@ -5,9 +5,10 @@ app = Flask(__name__)
 
 bootstrap = Bootstrap(app)
 
+
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
