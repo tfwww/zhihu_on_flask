006c2c564891185f31e6fe98c4bbf15f3cf4e9d6
wmzhong <zhongwm.net@gmail.com>
2016-03-12 20:35:42 +0800

    Reomved the lask.ext.bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
-from flask.ext.bootstrap import Bootstrap
+# from flask.ext.bootstrap import Bootstrap
 
 def create_app():
 	app = Flask(__name__)
-	bootstrap = Bootstrap(app)
+	# bootstrap = Bootstrap(app)
 
 	from .main import main as main_blueprint
 	app.register_blueprint(main_blueprint)
